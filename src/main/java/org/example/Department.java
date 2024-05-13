01230ecc916a0392e123b1be97d1206e285e103c
Christelle Aziz <christelleaziz24@gmail.com>
2024-05-13 14:35:05 -0400

    Creating the class of Department

---
+++ src/main/java/org/example/Department.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Department {
+    private String departmentId;
+    private String departmentName;
+    private static int nextId;
+
+    /**
+     * takes a department name and checks if it is valid or not
+     * @param departmentName the input department name
+     * @return if it is valid or nod
+     */
+    public boolean validDepartmentName(String departmentName) {
+        //TODO : to be implemented
+        return false;
+    }
+}
